43710cd3f951daf2342196940d9e604cc8be744c
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-03-20 02:37:02 -0300

    Começando a criação da aba funcionário
    foi feito o metodo getDados que busca os dados de uma api rest pela url
    a aba funcionários já está exibindo os dados recuperados.

--- src/app/funcionarios/funcionarios.page.ts
+++ src/app/funcionarios/funcionarios.page.ts
@@ -1,15 +1,30 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-funcionarios',
   templateUrl: './funcionarios.page.html',
-  styleUrls: ['./funcionarios.page.scss'],
+  styleUrls: ['./funcionarios.page.scss']
 })
 export class FuncionariosPage implements OnInit {
-
-  constructor() { }
-
+  
+  public itens:any;
+  constructor(public navCtrl:NavController, public http:HttpClient) {
+    this.getDados();
+  }
+  
   ngOnInit() {
   }
 
+  getDados(){
+    let url='https://jsonplaceholder.typicode.com/users';
+    let dado: Observable<any> = this.http.get(url);
+    dado.subscribe(result => {
+      this.itens = result
+    });
+
+  }
 }
