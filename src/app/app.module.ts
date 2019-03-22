c1edb815539e78988226a740b7d5557ce8105c35
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-03-22 00:11:37 -0300

    Consertando conflito de merge em app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,11 @@ import { ComponentModalAdicionarEventoPageModule } from './component-modal-adici
 
 
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-=======
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> df15f20cbf65d696113d1cdf72744a6d5042587e
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-<<<<<<< HEAD
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
-=======
   imports: [BrowserModule,
             IonicModule.forRoot(),
             AppRoutingModule,
@@ -33,7 +26,6 @@ import { ReactiveFormsModule } from '@angular/forms';
             ReactiveFormsModule,
             // DataFormModule
           ],
->>>>>>> df15f20cbf65d696113d1cdf72744a6d5042587e
   providers: [
     StatusBar,
     SplashScreen,
