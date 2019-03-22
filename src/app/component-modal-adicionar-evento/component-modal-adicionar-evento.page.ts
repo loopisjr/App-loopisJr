df15f20cbf65d696113d1cdf72744a6d5042587e
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-03-21 23:46:00 -0300

    Implementação de métodos:
    Implementação de método 'ngOnInit' para configuração do formulário.
    Implementação do método 'cadastrar' para cadastrar novos eventos.
    Criação do método 'limpar' para limpar os campos do formulário.

--- src/app/component-modal-adicionar-evento/component-modal-adicionar-evento.page.ts
+++ src/app/component-modal-adicionar-evento/component-modal-adicionar-evento.page.ts
@@ -1,5 +1,9 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavController } from '@ionic/angular';
+import { ModalController, NavController, AlertController } from '@ionic/angular';
+import { Evento } from 'src/model/Evento';
+
 
 @Component({
   selector: 'app-component-modal-adicionar-evento',
@@ -8,17 +12,57 @@ import { ModalController, NavController } from '@ionic/angular';
 })
 export class ComponentModalAdicionarEventoPage implements OnInit {
 
-  constructor(private nav:NavController,private modalCtrl:ModalController) { }
+  novoEvento : Evento;
+  form : FormGroup;
 
+  constructor(
+    private nav:NavController,
+    private modalCtrl:ModalController,
+    private http: HttpClient,
+    private alert: AlertController,
+    private formBuilder : FormBuilder
+  ){
+    this.novoEvento = new Evento();
+  }
+
+  //configurações do formulário do lado do server
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      titulo : [null, [Validators.required,Validators.minLength(6)]],
+      data: [null, Validators.required],
+      hora: [null, Validators.required],
+      descricao : [null]
+    })
   }
 
   fechar(){
     this.modalCtrl.dismiss();
   }
-
-  salvar(){
-    alert("Você salvou!");
+  
+  cadastrar(){
+    let url = "http://localhost:8081/eventos/";
+    this.http.post(url,this.form.value,{ observe : 'response'}).subscribe(res => {
+      console.log(this.form.value);
+      console.log(res);
+      //reseta os campos do form apos a requisição
+      this.form.reset();
+    },
+    (error: any) => this.alertNestaPagina("Falha","Falha ao cadastrar evento."));
+    
   }
 
+
+
+  async alertNestaPagina(titulo,msg){
+    const novoAlert = await this.alert.create({
+      header : titulo,
+      subHeader : msg,
+      buttons : ['OK']
+    });
+    await novoAlert.present();
+  }
+
+  limpar(){
+    this.form.reset();
+  }
 }
