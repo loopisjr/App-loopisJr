6ed1db13bfaa0f10595f03f600dd40e401afd5e0
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-03-23 11:01:40 -0300

    Configurações do novo modulo de cadastro de projetos
    Configurações para a modal de cadastro de projeto funcionar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'component-modal-adicionar-evento',
-   loadChildren: './component-modal-adicionar-evento/component-modal-adicionar-evento.module#ComponentModalAdicionarEventoPageModule' }
- 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ComponentModalAdicionarEventoPageModule } from './component-modal-adicionar-evento/component-modal-adicionar-evento.module'
-// import { ComponentModalAdicionarEventoPage } from './component-modal-adicionar-evento/component-modal-adicionar-evento.page'
-
-
+import { ComponentModalAdicionarEventoPageModule } from './component-modal-adicionar-evento/component-modal-adicionar-evento.module';
+import { ModalAdicionarProjetoPageModule } from './modal-adicionar-projeto/modal-adicionar-projeto.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -22,9 +18,9 @@ import { ReactiveFormsModule } from '@angular/forms';
             IonicModule.forRoot(),
             AppRoutingModule,
             ComponentModalAdicionarEventoPageModule,
+            ModalAdicionarProjetoPageModule,
             HttpClientModule,
             ReactiveFormsModule,
-            // DataFormModule
           ],
   providers: [
     StatusBar,
