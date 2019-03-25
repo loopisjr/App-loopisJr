92a102199358d6e5a0e18e28435fb122a598d364
bodescorp <glaymar2010@live.com>
2019-03-24 21:43:34 -0300

    integração tela de login com a Api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { UserService } from './../api/user.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
+import { FormBuilder, FormGroup, Validators, FormControl, FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -9,31 +11,34 @@ import { NavController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  result : any;
+export class HomePage implements OnInit {
+
+  result: any;
+
+
   constructor(
     private navCtrl: NavController,
     private userService: UserService,
+    public http: HttpClient
   ) { }
 
+  ngOnInit() {
+    
+  }
 
-  comecar() {
+  entrar() {
     this.navCtrl.navigateForward('tabs/principal');
 
   }
+  validar(form) {
+    let url = 'http://localhost:8081/funcionarios/login';
+    
+    console.log(form.value)
 
-  api_get() {
-    this.userService.getDados()
-      .then((response) => {
-        this.result = JSON.stringify(response);
-
-        console.log(this.result);
-      })
-      .catch(() => {
-        console.log('erro');
-      })
-
-  };
+    this.http.post(url,form.value,{ observe : 'response'}).subscribe(res => {
+      this.entrar();
+    },
+    (error: any) => console.log("Falha","Usuario não cadastrado"));
+    
+  }
 }
-
-
